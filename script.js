cd7b9c30994d8fc2edf91504852e89684bcd2ac3
Sarthak Pandey <pandeysarthak06@gmail.com>
2024-03-01 18:35:23 +0530

    change button

--- script.js
+++ script.js
@@ -3,12 +3,16 @@ var i = 0;
 var end = testing.length;
 function myFunction(){
     var txtElement = document.querySelector(".txt");
+    var buttoname = document.querySelector(".no");
+    buttoname.innerHTML = "Next";
     var newText = ''; 
     if (i<end){
         newText = testing[i];
         i+=1
     }
     else{
+        buttoname.innerHTML = "Refersh";
+        buttoname.setAttribute("onclick", "location.reload();"); 
         newText = 'Now i Am your friend just refresh the page and click on yes'
     }
     
